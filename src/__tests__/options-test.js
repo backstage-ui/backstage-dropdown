21783efd3168d5d4727dbc290669d74bc6ab4755
Andrews Medina <andrewsmedina@gmail.com>
2016-08-20 12:52:18 -0300

    add test for custom value

--- src/__tests__/options-test.js
+++ src/__tests__/options-test.js
@@ -6,12 +6,16 @@ import Option from '../option';
 describe('<Option />', () => {
   it('should has hover state', () => {
     const option = shallow(<Option />);
-
     expect(option.state('hover')).toBe(false);
   });
 
   it('should className be dropdown-option', () => {
     const wrapper = shallow(<Option />);
     expect(wrapper.hasClass('dropdown-option')).toBe(true);
+  });
+
+  it('should has value', () => {
+    const wrapper = shallow(<Option value="my value" />);
+    expect(wrapper.prop('value')).toBe('my value');
   });
 });
